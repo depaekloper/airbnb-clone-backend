b1dcd0e31d44a36f535a74f9aab7f3974386a663
Seungwoo <depaekloper@gmail.com>
2022-09-27 15:32:46 +0900

    #5.5 Foreign Keys

--- houses/models.py
+++ houses/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class House(models.Model):
 
-    """ Model Definition for Houses """
+    """Model Definition for Houses"""
 
     name = models.CharField(max_length=140)
     price_per_night = models.PositiveIntegerField(
@@ -16,6 +16,7 @@ class House(models.Model):
         default=True,
         help_text="Does this house allow pets?",
     )
+    owner = models.ForeignKey("users.User", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
