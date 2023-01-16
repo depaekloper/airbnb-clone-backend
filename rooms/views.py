86c1413f6c70429b4aebb4eb7cb2c9856bf96b3e
Seungwoo <depaekloper@gmail.com>
2023-01-16 16:46:04 +0900

    #11.18 Reviews

--- rooms/views.py
+++ rooms/views.py
@@ -173,6 +173,8 @@ class RoomDetail(APIView):
 
 
 class RoomReviews(APIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
     def get_object(self, pk):
         try:
             return Room.objects.get(pk=pk)
@@ -194,6 +196,18 @@ class RoomReviews(APIView):
             many=True,
         )
         return Response(serializer.data)
+
+    def post(self, request, pk):
+        serializer = ReviewSerializer(data=request.data)
+        if serializer.is_valid():
+            review = serializer.save(
+                user=request.user,
+                room=self.get_object(pk),
+            )
+            serializer = ReviewSerializer(review)
+            return Response(serializer.data)
+        else:
+            Response(serializer.errors)
 
 
 class AmenityReviews(APIView):
