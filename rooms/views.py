1dfcf2ebf40a445b577b647010101143d54a5510
Seungwoo <depaekloper@gmail.com>
2023-01-30 09:48:36 +0900

    #16.3 Create Amenity Test

--- rooms/views.py
+++ rooms/views.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.utils import timezone
 from django.db import transaction
 from rest_framework.views import APIView
-from rest_framework.status import HTTP_204_NO_CONTENT
+from rest_framework.status import HTTP_204_NO_CONTENT, HTTP_400_BAD_REQUEST
 from rest_framework.response import Response
 from rest_framework.exceptions import (
     NotFound,
@@ -35,7 +35,7 @@ class Amenities(APIView):
             amenity = serializer.save()
             return Response(AmenitySerializer(amenity).data)
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
 
 
 class AmenityDetail(APIView):
